1195c993f695efaeec3058605e2d7712720bad11
Peter Watson <48454993+sonofwatt84@users.noreply.github.com>
2021-12-22 15:04:46 -0500

    Update gribProcessor.R

--- dataProcessing/weatherData/gribConverter/gribProcessor.R
+++ dataProcessing/weatherData/gribConverter/gribProcessor.R
@@ -3,10 +3,10 @@ library("raster")
 library("rgdal")
 library("gdalUtils")
 
-# Establish File Path
-gTiffPath <- "LNCA98_KWBR_201709200400.tiff"
+# Establish File Path - Can be modified for for-loops
+gribPath <- "LNCA98_KWBR_201709200400.grib"
 
-gTiffNFO <- gdalinfo(gTiffPath)
+gTiffNFO <- gdalinfo(gribPath)
 
 # Import and Process Info from GeoTiff 
 bandNames <- gTiffNFO[grep("GRIB_ELEMENT",gTiffNFO)]
@@ -14,9 +14,9 @@ bandNames <- gsub("GRIB_ELEMENT=","",bandNames)
 bandNames <- gsub(" ","",bandNames, fixed = T)
 
 #Load Raster Stack from GRIB
-gTiffStack<-stack(gTiffPath)
+gribStack<-stack(gribPath)
 
 #Change Band Names in Raster Stack
-names(gTiffStack) <- bandNames
+names(gribStack) <- bandNames
 
 writeRaster(gribStack,filename = "gribTest.tif", format="GTiff", bylayer=T, suffix="names")
